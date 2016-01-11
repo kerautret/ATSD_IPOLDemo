968d7afb0ea852e7b0e8b073344863b4ea333838
Kerautret <kerautre@loria.fr>
2016-01-11 17:54:31 +0100

    add command args

--- app.py
+++ app.py
@@ -193,7 +193,12 @@ class app(base_app):
         inputWidth = image(self.work_dir + 'input_0.png').size[0]
         inputHeight = image(self.work_dir + 'input_0.png').size[1]
         command_args = ['lineDetectATS'] + \
-                       [ '-i', 'inputNG.pgm', '-o', 'res.eps', '-e', "outputContours.sdp"]
+                       [ '-i', 'inputNG.pgm', '-o', 'res.eps', '-e', "outputContours.sdp"] + \
+                       ['-s', str(self.cfg['param']['sigma'])] + \
+                       ['-l', str(self.cfg['param']['tmin'])] + \
+                       ['-h', str(self.cfg['param']['tmax'])] 
+                       
+        
         f = open(self.work_dir+"algoLog.txt", "a")
         cmd = self.runCommand(command_args, None, f)
         f.close()
