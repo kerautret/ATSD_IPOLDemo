1c5f4448c8f75e31e696e0d605f7de70840b2ac4
Kerautret <kerautre@loria.fr>
2016-02-11 17:23:28 +0100

    debug

--- app.py
+++ app.py
@@ -208,6 +208,14 @@ class app(base_app):
         cmd = self.runCommand(command_args, None, f)
         f.close()
 
+        ## ------
+        ## process 4: compute curvature on canny edges.
+        ## ------
+        #fInfoCurvature = open(self.work_dir+"algoCurvatureLog.txt", "a")
+        #command_args = ['mstCurvatureFilter', '-i', 'outputContours.txt', '-m','100']+\ 
+        #               ['-o', 'result', '-d', '2', '-M' ]
+        #cmd = self.runCommand(command_args, None, fInfoCurvature)
+        #fInfoCurvature.close()
 
 
 
@@ -221,13 +229,6 @@ class app(base_app):
         self.runCommand(command_args, None, fInfo)
         command_args = ['convert.sh', '-background', '#FFFFFF', '-flatten', \
                         'res_alphaThickSegments.eps', '-geometry', str(widthDisplay)+"x", 'res_alphaThickSegments.png']
-        self.runCommand(command_args, None, fInfo)
-        command_args = ['convert.sh', '-background', '#FFFFFF', '-flatten', \
-                        'resultCurvature.eps', '-geometry', str(widthDisplay)+"x", 'resultCurvature.png']
-        self.runCommand(command_args, None, fInfo)
-        command_args = ['convert.sh', '-background', '#FFFFFF', '-flatten', \
-                        'resultUnbiasedVarianceCurvature.eps', '-geometry', str(widthDisplay)+"x", \ 
-                        'resultUnbiasedVarianceCurvature.png']
         self.runCommand(command_args, None, fInfo)
 
 
